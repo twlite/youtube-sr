67980c82639daf8277837986e2dd3158fa522b95
ArnoldaZ <c.arnas123@gmail.com>
2025-07-02 19:34:14 +0300

    Update user agent

--- src/Util.ts
+++ src/Util.ts
@@ -121,7 +121,7 @@ class Util {
                 headers: Object.assign(
                     {},
                     {
-                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:140.0) Gecko/20100101 Firefox/140.0"
                     },
                     requestOptions?.headers || {}
                 )
